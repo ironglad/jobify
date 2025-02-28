b63b7864803e95e42a8629923cc68d3eab4fcc27
Varun <varun@example.com>
2025-03-01 02:10:19 +0530

    login page done

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div className=" flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          action=""
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">LogIn</h1>
+  
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input type="email" placeholder="email" />
+          </div>
+ 
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input type="password" placeholder="password" />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                type="radio"
+                value="student"
+                name="role"
+                className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                type="radio"
+                name="role"
+                value="recruiter"
+                className="cursor-pointer" />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button type="submit" className="w-full my-4 bg-[#C5BAFF] hover:bg-[#aea2ec]">LogIn</Button>
+        <div>
+        <span className="text-sm">Don't have an account? <Link to="/signup" className="text-blue-600">SignUp</Link></span>
+        </div>
+        </form>
+
+      </div>
+    </div>
   )
 }
 
